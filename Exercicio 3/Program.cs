f4eb61c30760713bb7f9ea68e5d0e4594cfa2e1c
alvxres <42217246+alvxres@users.noreply.github.com>
2018-08-25 09:38:03 -0300

    Add files via upload

---
+++ Exercicio 3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o número de horas trabalhadas");
+            double hora = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o valor do salário mínimo");
+            double salmin = double.Parse(Console.ReadLine());
+            double valhora = ((salmin)/0.10);
+            double salbrut = (hora * valhora);
+            double salfinal = (salbrut - 0.03);
+            Console.WriteLine("O valor do salário a receber é: {0}", salfinal.ToString("F2"));
+            Console.ReadKey();
+
+
+        }
+    }
+}
