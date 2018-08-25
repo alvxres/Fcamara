e79608c71dbcb1303823ec4566bc038a9ec12c8a
alvxres <42217246+alvxres@users.noreply.github.com>
2018-08-25 09:39:46 -0300

    Add files via upload

---
+++ Exercicio 6/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ModeloRetangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double QtPiso, AreaLocal, AreaPiso, QtRodape ;
+            Retangulo r = new Retangulo();
+            Console.WriteLine("Insira a comprimento do local");
+            r.LadoA = double.Parse(Console.ReadLine());
+            Console.WriteLine("Insira a largura do local");
+            r.LadoB = double.Parse(Console.ReadLine());
+            Console.WriteLine("Insira o comprimento do piso");
+            r.LadoApiso = double.Parse(Console.ReadLine());
+            Console.WriteLine("Insira o largura do piso");
+            r.LadoBpiso = double.Parse(Console.ReadLine());
+            Console.WriteLine("Insira o comprimento do rodapé");
+            r.LadoArodape = double.Parse(Console.ReadLine());
+
+
+            AreaLocal = r.AreaCalc(r.LadoA, r.LadoB);
+            AreaPiso = r.AreaPis(r.LadoApiso, r.LadoBpiso);
+
+            QtPiso = AreaLocal / AreaPiso;
+
+            QtRodape = ((r.LadoA / r.LadoArodape) * 2) + ((r.LadoB / r.LadoArodape) * 2);
+
+
+
+
+
+
+
+
+
+
+
+            Console.WriteLine("A quantidade necessária de pisos/rodapés necessária para a área informada é{0},{1}: ", QtPiso, QtRodape);
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Exercicio 6/Retangulo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModeloRetangulo
+{
+    class Retangulo
+    {
+        public double LadoA { get; set; }
+        public double LadoB { get; set; }
+        public double LadoApiso { get; set; }
+        public double LadoBpiso { get; set; }
+        public double LadoArodape { get; set; }
+ 
+
+        public void MudaLado(double altret, double largret)
+        {
+            Console.WriteLine("Insira a altura do local");
+            LadoA = double.Parse(Console.ReadLine());
+            Console.WriteLine("Insira a largura do local");
+            LadoB = double.Parse(Console.ReadLine());
+            Console.WriteLine("Valores alterados.");
+            return;
+        }
+        public string RetornaLado(double altret, double largret)
+        {
+            string a = LadoA + " " + LadoB;
+            return a;
+        }
+        public double AreaCalc(double altret, double largret)
+        {
+            return LadoA * LadoB;
+        }
+        public double PerimetroCalc(double altret, double largret)
+        {
+            return (2 * (largret * altret));
+        }
+        public double AreaPis(double altpis, double largpis)
+        {
+            return LadoApiso * LadoBpiso;
+        }
+    }
+}
+    
