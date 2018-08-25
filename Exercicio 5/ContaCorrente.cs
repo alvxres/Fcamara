2d1905f4ff0e07dcc0f371669ebb4bba30e6611a
alvxres <42217246+alvxres@users.noreply.github.com>
2018-08-25 09:39:14 -0300

    Add files via upload

---
+++ Exercicio 5/ContaCorrente.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp5
+{
+    class ContaCorrente
+    {
+        int numconta { get; set; }
+        String nomecorrentista { get; set; }
+        double saldo { get; set; }  
+
+        public void Construtor(int numconta, String nomecorrentista, double saldo)
+        {
+            numconta = 12345;
+            nomecorrentista = "José de Tal";
+            saldo = 0.00;
+        }
+        public string Alterarnome()
+        { return nomecorrentista; }
+        public double Deposito(double valordeposito)
+        { return saldo + valordeposito; }
+        public double Saque(double saque)
+        { return saldo - saque; }
+
+       
+    }
+}
